ffc2cfdf253921362943d62c088b0d8c3bb17ade
jonib <jonib@10.164.49.110>
2019-09-24 08:39:00 +0200

    Creada estructura del proyecto

---
+++ VentanaCerrar/src/MainWindow.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public MainWindow() {
+		this.setTitle("Mi primera ventana");
+		this.setSize(320, 240);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+	}
+
+	public static void main(String[] args) {
+		MainWindow mainWindow = new MainWindow();
+		mainWindow.setVisible(true);
+		
+		
+	}
+
+}
